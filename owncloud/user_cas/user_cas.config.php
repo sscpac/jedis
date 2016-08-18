3fc963ecafad98a5274b616be84bda7d75af0e12
Reid Wakida <rwakida@gmail.com>
2016-08-18 11:43:54 -1000

    Adds user_cas application initial configuration settings configured to use jedis-dev CAS environment

---
+++ owncloud/user_cas/user_cas.config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'cas_autocreate' => 'on',
+  'cas_force_login' => 'on',
+  'cas_link_to_ldap_backend' => '',
+  'cas_update_user_data' => '',
+  'cas_default_group' => '',
+  'cas_protected_groups' => '',
+  'cas_email_mapping' => '',
+  'cas_displayName_mapping' => '',
+  'cas_group_mapping' => '',
+
+  'cas_server_version' => '2.0',
+  'cas_server_hostname' => 'jedis-dev.spawar.navy.mil',
+  'cas_server_port' => '443',
+  'cas_server_path' => '/cas',
+  'cas_debug_file' => '/tmp/user_cas.debug.log',
+  'cas_cert_path' => '/var/www/owncloud/resources/config/jedis-dev.spawar.navy.mil.crt',
+  'cas_php_cas_path' => '/usr/local/lib/php/CAS.php',
+  'cas_service_url' => 'https://jedis-dev.spawar.navy.mil/calendar',
+);
